36e3ff91eaa6be449ce2d4b926754484569d4d26
vitaliyborodiy <vitalii@email.ua>
2016-04-11 11:14:15 +0300

    syntax refactor

--- models/User.js
+++ models/User.js
@@ -13,23 +13,22 @@ var User = thinky.createModel("users", {
 User.define("creteWithPassword", function (next) {
     var user = this;
 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-        return next(err);
-    }
-user.salt = salt;
-// hash the password using our new salt
-bcrypt.hash(user.hashedPassword, salt, (err, hash) => {
-    if (err) {
-        return next(err);
-    }
-
-// override the cleartext password with the hashed one
-user.hashedPassword = hash;
-next(null);
-});
-});
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        user.salt = salt;
+        // hash the password using our new salt
+        bcrypt.hash(user.hashedPassword, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            // override the cleartext password with the hashed one
+            // user.hashedPassword = hash;
+        next(null);
+        });
+    });
 });
 
 User.define("verifyPassword", function(candidatePassword, cb) {
